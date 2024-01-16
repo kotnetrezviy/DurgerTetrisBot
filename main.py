aad4d4816c27569aec221e5a218bd7b6a733a37e
kotnetrezviy <kotnetrezviy@mail.ru>
2024-01-16 11:03:24 +0300

    main

--- main.py
+++ main.py
@@ -2,30 +2,30 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher,types
-from aiogram.types.web_app_info import WebAppInfo
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardMarkup
+from aiogram.types.web_app_info import WebAppInfo
 
-import config
+import constants
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(config.BOT_TOKEN)
+bot = Bot(token=constants.BOT_TOKEN)
 # Диспетчер
 dp = Dispatcher()
 
 # Хэндлер на команду /start
-@dp.message(Command=['start'])
+@dp.message(Command("start"))
 async def start(message: types.Message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Open url', web_app=WebAppInfo(url='https://kotnetrezviy.github.io/DurgerTetrisBot')))
-    await message.answer("Hello!")
+    start_markup = types.InlineKeyboardMarkup(
+        inline_keyboard=[[types.InlineKeyboardButton(text = "Play", web_app=WebAppInfo(url="https://github.com"))]])
+    await message.answer("Welcome to @DurgetTetrisBot", reply_markup=start_markup)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
